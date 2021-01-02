129b761e671253bb11571d75b73d50a35626d4eb
Alexandra Frydl <me@alexfrydl.com>
2021-01-01 22:56:59 -0500

    Add license info

--- af-macros/src/util.rs
+++ af-macros/src/util.rs
@@ -1,3 +1,9 @@
+// Copyright © 2020 Alexandra Frydl
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 /// Runs a block of code and catches `?` operator returns.
 ///
 /// This is just a cleaner version of creating a closure and immediately calling
